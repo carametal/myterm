4e51d004d5b24724e37f91c6098d2a15fdeeedb8
carametal <mkatsdevelop@gmail.com>
2025-03-16 16:37:30 +0900

    mytermコマンドの作成とsplit-paneサブコマンドの実装

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World.")
+	app := &cli.App{
+		Name:  "myterm",
+		Usage: "Integration cli commands",
+		Commands: []*cli.Command{
+			{
+				Name:    "split-pane",
+				Aliases: []string{"split", "sp"},
+				Usage:   "Split pane",
+				Action: func(cCtx *cli.Context) error {
+					err := exec.Command("wezterm", "cli", "split-pane", "--horizontal").Run()
+					if err != nil {
+						log.Fatal(err)
+					}
+					return nil
+				},
+			},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
