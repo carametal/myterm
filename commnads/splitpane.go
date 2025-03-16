98d759a8d56dde08981ba6b9767fe6bd44fa8db2
carametal <mkatsdevelop@gmail.com>
2025-03-16 19:45:38 +0900

    splitpane.goのリファクタリング

--- commnads/splitpane.go
+++ commnads/splitpane.go
@@ -7,35 +7,40 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func f(args ...string) error {
+	err := exec.Command("wezterm", args...).Run()
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+var splitPaneHorizontal = &cli.Command{
+	Name:    "horizontal",
+	Aliases: []string{"h"},
+	Usage:   "Split pane horizontal",
+	Action: func(cCtx *cli.Context) error {
+		return f("cli", "split-pane", "--horizontal")
+	},
+}
+
+var splitPaneVertical = &cli.Command{
+	Name:    "vertical",
+	Aliases: []string{"v"},
+	Usage:   "Split pane vertical",
+	Action: func(cCtx *cli.Context) error {
+		return f("cli", "split-pane")
+	},
+}
+
 var SplitPane = &cli.Command{
 	Name:     "split-pane",
 	Aliases:  []string{"split", "sp"},
-	Usage:    "Split pane",
+	Usage:    "Split pane in the terminal",
 	HideHelp: true,
 	Subcommands: []*cli.Command{
-		{
-			Name:    "horizontal",
-			Aliases: []string{"h"},
-			Usage:   "Split pane horizontal",
-			Action: func(cCtx *cli.Context) error {
-				err := exec.Command("wezterm", "cli", "split-pane", "--horizontal").Run()
-				if err != nil {
-					log.Fatal(err)
-				}
-				return nil
-			},
-		},
-		{
-			Name:    "vertical",
-			Aliases: []string{"v"},
-			Usage:   "Split pane vertical",
-			Action: func(cCtx *cli.Context) error {
-				err := exec.Command("wezterm", "cli", "split-pane").Run()
-				if err != nil {
-					log.Fatal(err)
-				}
-				return nil
-			},
-		},
+		splitPaneHorizontal,
+		splitPaneVertical,
 	},
 }
